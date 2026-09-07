87b07a6e722e784f238dd656883ddbeb32e82c95
prakashwaddar628 <prakashwaddar628@gmail.com>
2026-09-07 14:36:38 +0530

    Initial Flutter app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('My Flutter App')),
+        body: const Center(
+          child: Text('Hello, World!', style: TextStyle(fontSize: 24)),
+        ),
+      ),
+    );
+  }
+}
